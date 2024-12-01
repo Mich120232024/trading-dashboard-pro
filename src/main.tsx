980c6768a714d6a94aecaf000d3c4433827ccb3d
Mich120232024 <132930015+Mich120232024@users.noreply.github.com>
2024-12-01 18:45:09 +0100

    Fix main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
