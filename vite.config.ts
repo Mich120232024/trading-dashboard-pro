6b18ef28e83e070736dfb928f200a50ec956c841
MT <mt@gzcim.com>
2024-12-01 19:34:41 +0100

    1013

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,10 +9,17 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    host: true, // Add this line to listen on all network interfaces
-    strictPort: true, // Add this to ensure it uses exactly this port
+    host: true,
+    strictPort: true,
   },
   css: {
-    postcss: "./postcss.config.cjs",
+    modules: {
+      localsConvention: "camelCase",
+    },
+    preprocessorOptions: {
+      postcss: {
+        config: true,
+      },
+    },
   },
 });
