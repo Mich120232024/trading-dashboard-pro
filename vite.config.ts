c1e96d2b192c3c4e0465df1c82ce0e49b90306b6
Mich120232024 <132930015+Mich120232024@users.noreply.github.com>
2024-12-02 09:09:19 +0100

    Add Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
   server: {
     port: 5173,
-    host: true,
-    strictPort: true,
+    open: true
   },
   css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-    preprocessorOptions: {
-      postcss: {
-        config: true,
-      },
-    },
-  },
-});
+    postcss: './postcss.config.js'
+  }
+})
